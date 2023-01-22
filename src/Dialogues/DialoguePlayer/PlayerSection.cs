03350b16a5bbe7b4d720bc13e9d4c7affce7a553
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-01-22 09:58:06 +0100

    dialogue player basics

---
+++ src/Dialogues/DialoguePlayer/PlayerSection.cs
@@ -0,0 +1,22 @@
+using Godot;
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace PirateInBetween.Game.Dialogue
+{
+	public class PlayerSection : Panel
+	{
+		#region Paths
+		[Export] private NodePath _choicesVBoxPath;
+		[Export] private NodePath _speakerNamePath;
+
+		#endregion
+
+
+	}
+}

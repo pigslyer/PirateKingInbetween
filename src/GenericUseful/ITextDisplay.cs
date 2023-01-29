c4073bd2c78b93ed147bc218d9c56e17d160e879
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-01-29 08:44:34 +0100

    Text with fancy little appear and disappear animation

---
+++ src/GenericUseful/ITextDisplay.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace PirateInBetween
+{
+	/// <summary>
+	/// A text display that can appear and disappear on command.
+	/// </summary>
+	public interface ITextDisplay
+	{
+		void Appear(Node parent, Vector2 globalPosition, string text);
+		void Disappear();
+		string CurrentText();
+	}
+}

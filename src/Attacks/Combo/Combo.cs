4e629a8ca684a62a3bf90821acb5fd6b8b4d6fc9
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-02-06 17:05:21 +0100

    improved combo's Async functions

--- src/Attacks/Combo/Combo.cs
+++ src/Attacks/Combo/Combo.cs
@@ -13,24 +13,47 @@ namespace PirateInBetween.Game.Combos
 {
 	public abstract class Combo
 	{
+		protected class TaskData
+		{ }
+
 		// genuinely surprised Godot doesn't throw a fit, this is for interpolation.
 		private static readonly SceneTreeTween _tween = new SceneTreeTween();
 
 		private static readonly (Combo combo, ComboAttr attr)[] _combos = ReflectionHelper.GetInstancesWithAttribute<Combo, ComboAttr>();
 		public static ReadOnlyCollection<(Combo combo, ComboAttr attr)> Combos => new ReadOnlyCollection<(Combo combo, ComboAttr attr)>(_combos);
 
-		private List<TaskCompletionSource<ICombatFrameData>> _tasks = new List<TaskCompletionSource<ICombatFrameData>>();
+		protected ICombatFrameData CurrentData { get; private set; }
+		protected IComboExecutor CurrentExecutor { get; private set; }
+		private List<TaskCompletionSource<TaskData>> _tasks = new List<TaskCompletionSource<TaskData>>();
+
+		private TaskCompletionSource<float> _comboFinishTask = new TaskCompletionSource<float>();
 
 		private bool _executingCombo = false;
+		private List<Action> _onFinished = new List<Action>();
+
 		public async Task ExecuteCombo(IComboExecutor executor, ICombatFrameData startingData)
 		{
-
 			_executingCombo = true;
+			CurrentData = startingData;
+			CurrentExecutor = executor;
 
-			await BeginCombo(executor, startingData);
+			await BeginCombo(executor);
+
+			CurrentData = null;
+			CurrentExecutor = null;
+
+			_onFinished.ForEach(action => action());
+			_onFinished.Clear();
 
 			_executingCombo = false;
+
 		}
+
+		/// <summary>
+		/// Performs action when combo finishes. This list is cleared when the combo finishes, meaning registering things before it begins is valid.
+		/// </summary>
+		/// <param name="what"></param>
+		public void OnFinished(Action what) => _onFinished.Add(what);
 
 		public void GiveFrameData(ICombatFrameData data)
 		{
@@ -39,18 +62,26 @@ namespace PirateInBetween.Game.Combos
 				throw new InvalidOperationException("I swear to god");
 			}
 
-			IEnumerable<TaskCompletionSource<ICombatFrameData>> tasks = _tasks.ToArray();
+			CurrentData = data;
+
+			Update();
+		}
+
+		private void Update()
+		{
+			IEnumerable<TaskCompletionSource<TaskData>> tasks = _tasks.ToArray();
 			_tasks.Clear();
 
 			foreach (var task in tasks)
 			{
-				task.SetResult(data);
+				task.SetResult(null);
 			}
+
 		}
 
-		protected async Task<ICombatFrameData> GetNextFrameData()
+		protected async Task<TaskData> AwaitNextFrameData()
 		{
-			_tasks.Add(new TaskCompletionSource<ICombatFrameData>());
+			_tasks.Add(new TaskCompletionSource<TaskData>());
 			return await _tasks.Last().Task;
 		}
 
@@ -61,27 +92,64 @@ namespace PirateInBetween.Game.Combos
 		/// <param name="what"></param>
 		/// <param name="startingData"></param>
 		/// <returns></returns>
-		protected async Task DoFor(float time, Action<float, float, float, ICombatFrameData> what, ICombatFrameData startingData)
+		protected async Task DoFor(float time, Action<float, float, float> what)
 		{
 			float t = 0f;
-			ICombatFrameData data = startingData ?? await GetNextFrameData();
-
+			
 			while (t < time)
 			{
-				what(t, Mathf.Max(data.Delta, time - t), time, data);
+				what(t, Mathf.Max(CurrentData.Delta, time - t), time);
 
-				t += data.Delta;
+				t += CurrentData.Delta;
 
-				data = await GetNextFrameData();
+				await AwaitNextFrameData();
 			}
 		}
 
+		protected async Task WaitFor(float time) => await DoFor(time, (a, b, c) => {});
+
+		protected async Task DealDamageFor(FloatInterval time, ComboExecutorDamageDealers area, DamageData data)
+		{
+			await WaitFor(time.Start);
+			
+			CurrentExecutor.DealDamage(area, data);
+			
+			await WaitFor(time.Delta);
+
+			CurrentExecutor.StopDealingDamage(area);
+		}
+
+		/// <summary>
+		/// Essentially an overload of <see cref="DoFor"/> which directly cubically interpolates the given value and sets it to the target.
+		/// </summary>
+		/// <param name="from"></param>
+		/// <param name="delta"></param>
+		/// <param name="setter"></param>
+		/// <param name="time"></param>
+		/// <param name="startingData"></param>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		protected async Task CubicInterpFor<T>(T from, T delta, Action<T> setter, float time)
+		{
+			float t = 0f;
+
+			while (t < time)
+			{
+				setter(GetCubicInterp<T>(from, delta, t, time));
+
+				t += CurrentData.Delta;
+				await AwaitNextFrameData();
+			}
+		}
+
+		protected async Task CubicInterpFor(Vector2 from, Vector2 to, Action<Vector2> setter, float time) => await CubicInterpFor<Vector2>(from, to - from, setter, time);
+		protected async Task CubicInterpFor(float from, float to, Action<float> setter, float time) => await CubicInterpFor<float>(from, to - from, setter, time);
 
 		protected static T GetCubicInterp<T>(T init, T delta, float elapsed, float duration)
 		{
 			return (T)_tween.InterpolateValue(init, delta, elapsed, duration, Tween.TransitionType.Cubic, Tween.EaseType.InOut);
 		}
 
-		protected abstract Task BeginCombo(IComboExecutor executor, ICombatFrameData startingData);
+		protected abstract Task BeginCombo(IComboExecutor executor);
 	}
 }

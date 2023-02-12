c1e2975676d967f7bfb6e2617c2e23e0d8e3562a
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-02-12 22:18:47 +0100

    player damage handler now properly handles stun and knockback (probably) states

--- src/Player/Behaviours/PlayerDamageHandler.cs
+++ src/Player/Behaviours/PlayerDamageHandler.cs
@@ -11,6 +11,8 @@ namespace PirateInBetween.Game.Player.Behaviours
 {
 	public class PlayerDamageHandler : PlayerBehaviour
 	{
+		private float _stunTimer = 0f;
+
 		public DamageReaction TakeDamage()
 		{
 			if (IsEnabled())
@@ -21,14 +23,40 @@ namespace PirateInBetween.Game.Player.Behaviours
 			return DamageReaction.Zero;
 		}
 
-		public void StunFor(float time)
+		public void StunFor(float time, PlayerCurrentFrameData data)
 		{
-			
+			if (time > 0f)
+			{
+				data.Velocity = Vector2.Zero;
+				_stunTimer = time;
+				ForceBehaviourChangesDisabled();
+
+				SetBehaviourChangesDisabled(true);
+				SetBehavioursEnabled(Behaviours.Controllable, false);
+			}
+		}
+
+		public void KnockbackFor(Vector2 velocity, PlayerCurrentFrameData data) => data.Velocity = velocity;
+
+		public void DisableStun()
+		{
+			_stunTimer = 0f;
+
+			SetBehaviourChangesDisabled(false);
+			SetBehavioursEnabled(Behaviours.Controllable, true);
 		}
 
 		public override void Run(PlayerCurrentFrameData data)
-		{ 
-
+		{
+			if (_stunTimer > 0f)
+			{
+				_stunTimer -= data.Delta;
+		
+				if (_stunTimer <= 0f)
+				{
+					DisableStun();
+				}
+			}
 		}
 	}
 }

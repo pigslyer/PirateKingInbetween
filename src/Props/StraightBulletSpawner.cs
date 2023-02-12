f525d432e4ea2b878ffdc0e60f362f3386eefa22
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-02-12 22:19:06 +0100

    bullet spawner now has inputtable damage and stun durations

--- src/Props/StraightBulletSpawner.cs
+++ src/Props/StraightBulletSpawner.cs
@@ -13,6 +13,9 @@ namespace PirateInBetween.Game.Props
 	{
 		private PackedScene _bullet = ReflectionHelper.LoadScene<StraightBullet>();
 
+		[Export] private int _damageAmount = 10;
+		[Export] private float _stunDuration = 2f;
+
 		[Export] private float _firingInterval = 10f;
 		[Export] private float _fireAngle = 0f;
 		[Export] private float _velocity = 100f;
@@ -29,7 +32,7 @@ namespace PirateInBetween.Game.Props
 			while (!IsQueuedForDeletion())
 			{
 				StraightBullet bullet = _bullet.Instance<StraightBullet>();
-				bullet.Initialize(PhysicsLayers.PlayerHittable, 10, _fireAngle.ToVectorDeg(_velocity));
+				bullet.Initialize(PhysicsLayers.PlayerHittable, new DamageAmount(_damageAmount, _stunDuration), _fireAngle.ToVectorDeg(_velocity));
 				bullet.Shoot(GlobalPosition, MovingParent.GetMovingParentOf(this));
 
 				await this.WaitFor(_firingInterval);

ce0b4a54eebe198dd638a806aa7248b5ed605291
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-03-14 22:32:20 +0100

    basic enemy now uses all that ai stuff

--- src/Enemies/BasicMeleeEnemy/BasicMeleeBehaviour.cs
+++ src/Enemies/BasicMeleeEnemy/BasicMeleeBehaviour.cs
@@ -13,12 +13,23 @@ namespace PirateInBetween.Game.Enemies.Behaviours
 {
 	public class BasicMeleeBehaviour : GroundlingBehaviour
 	{
+		private const float REACTION_TIME = 1.5f;
+
 		protected override void Execute()
 		{
-			if (TrySeeOpponent(DamageDealerTargettingArea.Front, out var _))
+			if (TrySeeOpponent(DamageDealerTargettingArea.Front, REACTION_TIME, out var _))
 			{
 				ExecuteCombo<BasicCombo1>();
 
+				return;
+			} 
+			
+			if (TrySeeOpponent(DamageDealerTargettingArea.Front, out var _))
+			{
+				CurrentData.IsAboutToAttack = true;
+				StopWandering(REACTION_TIME * 2);
+
+				Wander(PatrolMaxSpeed, PatrolMovementAcceleration * 2, (2f, 6f), (6f, 8f), false);
 				return;
 			}
 

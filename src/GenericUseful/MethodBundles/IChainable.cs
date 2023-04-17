1ce1d0ae160bbd0f00806396832a461162101a06
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-04-17 06:25:03 +0200

    initial godot4 commit

--- src/GenericUseful/MethodBundles/IChainable.cs
+++
@@ -1,9 +0,0 @@
-
-/// <summary>
-/// Represents a monadic(?) class which can form a single link in a chain.
-/// </summary>
-/// <typeparam name="T"></typeparam>
-public interface IChainable<T>
-{
-	IChainable<T> Chain(T data);
-}

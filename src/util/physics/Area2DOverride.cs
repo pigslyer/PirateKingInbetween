631f377fc62c479e9c017376bcefe26ad6146d31
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-04-28 09:10:17 +0200

    added area2d override

---
+++ src/util/physics/Area2DOverride.cs
@@ -0,0 +1,26 @@
+using Godot;
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Pigslyer.PirateKingInbetween.Util.Physics
+{
+	public partial class Area2DOverride : Area2D
+	{
+		public new PhysicsLayers2D CollisionLayer
+		{
+			get => (PhysicsLayers2D)base.CollisionLayer;
+			set => base.CollisionLayer = (uint)value;
+		}
+
+		public new PhysicsLayers2D CollisionMask
+		{
+			get => (PhysicsLayers2D)base.CollisionMask;
+			set => base.CollisionMask = (uint)value;
+		}
+	}
+}

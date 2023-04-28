a9551be11d2dd0dfb965b0361a3fca26c3c823b0
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-04-28 09:11:27 +0200

    reordered usings to be alphabetic :)

--- src/util/GlobalUsing.cs
+++ src/util/GlobalUsing.cs
@@ -1,6 +1,6 @@
 
-global using Pigslyer.PirateKingInbetween.Util.Singletons;
-global using Pigslyer.PirateKingInbetween.Util.Generic;
 global using Pigslyer.PirateKingInbetween.Util.FrameData;
+global using Pigslyer.PirateKingInbetween.Util.Generic;
 global using Pigslyer.PirateKingInbetween.Util.Input;
 global using Pigslyer.PirateKingInbetween.Util.Physics;
+global using Pigslyer.PirateKingInbetween.Util.Singletons;
